cde5610e32400abe904f358fddeea281468086d8
Josh Harris <joshuaharris51@gmail.com>
2016-01-19 17:47:45 +0000

    Added basic model for deadman switch

--- deadman/settings.py
+++ deadman/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'deadmanapp.apps.DeadmanappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- deadmanapp/models.py
+++ deadmanapp/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class ContactNumber(models.Model):
+    contact_name = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=50)
+
+class DeadmanSwitch(models.Model):
+    contact = models.ForeignKey(ContactNumber, on_delete=models.CASCADE)
+    interval = models.IntegerField(default=30)
+    last_alive = models.DateTimeField()
+    disabled = models.BooleanField(default=false)
 # Create your models here.
